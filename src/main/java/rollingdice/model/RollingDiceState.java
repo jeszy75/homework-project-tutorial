99951ad94d4fbc1513acbed10a830b494c43f19c
Péter Jeszenszky <jeszenszky.peter@inf.unideb.hu>
2024-06-20 21:43:31 +0200

    fix: relocate misplaced closing curly brace in Javadoc comment

--- src/main/java/rollingdice/model/RollingDiceState.java
+++ src/main/java/rollingdice/model/RollingDiceState.java
@@ -66,8 +66,8 @@ public class RollingDiceState implements State<Direction> {
     }
 
     /**
-     * {@return a copy of the dice. A copy is returned that does not allow the
-     * outside world to change the internal state of the object.}
+     * {@return a copy of the dice} A copy is returned that does not allow the
+     * outside world to change the internal state of the object.
      */
     public Dice getDice() {
         return dice.clone();
